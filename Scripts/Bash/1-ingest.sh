93308e7193606479f0adb6594a6ef323fcce1420
0xKanu <kanuonyedikachi7@gmail.com>
2025-09-06 14:24:13 +0100

    Add ingest scripts and file structure

---
+++ Scripts/Bash/1-ingest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export URL="https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
+
+mkdir -p raw
+
+filepath="raw/$(basename "$URL")"
+
+curl -o "$filepath" "$URL"
+
+if [ -f "$filepath" ]; then
+	echo "File successfully saved in $filepath"
+else
+	echo "File not saved correctly."
+fi
